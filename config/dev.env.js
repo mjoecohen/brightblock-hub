1bd98f773a361ea4957319a9fae25ef051124347
Mike Cohen <mjoecohen@gmail.com>
2019-01-22 14:09:44 +0000

    stripped out auction files

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,17 +1,9 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-const abi = require('./abi/ArtMarket')
-// console.log(abi)
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   VUE_APP_DEBUG_MODE: 'false',
-  VUE_APP_TOK_BOX_API_KEY: '46171452',
-  VUE_APP_SHAPE_SHIFT_URL: '"https://cors.shapeshift.io"',
-  VUE_APP_ETH_GATEWAY_URL: '"http://localhost:8191"',
-  VUE_APP_BTC_GATEWAY_URL: '"https://api.brightblock.org"',
-  VUE_APP_SEARCH_INDEX_URL: '"http://localhost:8193"',
-  VUE_APP_ETHEREUM_NETWORK: '"Ganache"',
-  VUE_APP_ETHEREUM_ABI: '\'' + JSON.stringify(abi) + '\'',
+  VUE_APP_BTC_GATEWAY_URL: '"https://api.brightblock.org"'
 })
